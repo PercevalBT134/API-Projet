dcfd12e5ada5075188915c58c73275b13e3520f0
PercevalBT134 <perceval.baillythomas@ynov.com>
2025-12-05 09:33:08 +0100

    Fix système role 2

--- middleware/roles.js
+++ middleware/roles.js
@@ -1,8 +1,9 @@
 export const authorizeRoles = (...allowedRoles) => {
   return (req, res, next) => {
     if (!req.user) return res.status(401).json({ error: "Non authentifié" });
-    if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ error: "Accès interdit pour ce rôle" });
+    role = req.user.role.trim();
+    if (!allowedRoles.map(r => r.toLowerCase()).includes(role.toLowerCase())) {
+    return res.status(403).json({ error: "Accès interdit pour ce rôle" });
     }
     next();
   };
